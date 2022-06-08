379568639e4eaf89f8d2a19c894db5a15575084a
chrisrod46 <chrisrod46@csu.fullerton.edu>
2022-06-08 14:37:06 -0700

    Initial struct made

---
+++ Algo.swift
@@ -0,0 +1,14 @@
+//
+//  Algo.swift
+//  Political Iniquisition (iOS)
+//
+//  Created by csuftitan on 6/8/22.
+//
+
+import Foundation
+
+struct algo
+{
+    //add code to promt user about questions
+    //regarding political views
+}
