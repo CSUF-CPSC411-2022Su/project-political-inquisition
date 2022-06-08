a92200f0482c260733c7434383e66eda358653d2
chrisrod46 <chrisrod46@csu.fullerton.edu>
2022-06-08 14:15:25 -0700

    Initial Commit

---
+++ Shared/Political_IniquisitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Political_IniquisitionApp.swift
+//  Shared
+//
+//  Created by csuftitan on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Political_IniquisitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
