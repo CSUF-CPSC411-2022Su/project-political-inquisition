d4310c003e4f50a1901dfdea6f9f09b8eef5d399
chrisrod46 <chrisrod46@csu.fullerton.edu>
2022-06-28 15:32:42 -0700

    ContentView2

---
+++ ContentView2.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by csuftitan on 6/8/22.
+//
+
+import SwiftUI
+
+struct ContentViewAlgo: View {
+    
+    @StateObject var quiz = Algo()
+    
+    var body: some View{
+        Image("oranges")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 100)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+            .stroke(lineWidth: 5)
+            .foregroundColor(.black))
+        .shadow(radius: 5)
+        TabView {
+            QuizView()
+                .tabItem {
+                            Image(systemName: "info")
+                            Text("Quiz")
+                        }
+                    Result()
+                .tabItem {
+                    Image(systemName: "info")
+                    Text("Result")
+                }
+
+        }.environmentObject(quiz)
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentViewAlgo()
+    }
+}
